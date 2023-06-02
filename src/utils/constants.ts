f2df49c30e424caa4edb7d455008fa6af6d43e69
sudarshan1998 <sudarshandhakal21@gmail.com>
2023-06-02 15:47:11 +0545

    update the indentation

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,30 +4,30 @@
 
  //Header for the cors policy
 const headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': false,
-  }
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Credentials': false,
+}
 
 export const successResponse = (body: {}) => {
-    return {
-        statusCode: 200,
-        headers,
-        body: JSON.stringify({body})
-    }
+	return {
+		statusCode: 200,
+		headers,
+		body: JSON.stringify({body})
+	}
 }
 
 export const interServerError = (body: {}) => {
-    return {
-        statusCode: 500,
-        headers,
-        body: JSON.stringify({body})
-    }
+	return {
+		statusCode: 500,
+		headers,
+		body: JSON.stringify({body})
+	}
 }
 
 export const otherError = (body: {}) => {
-    return {
-        statusCode: 400,
-        headers,
-        body: JSON.stringify({body})
-    }
+	return {
+		statusCode: 400,
+		headers,
+		body: JSON.stringify({body})
+	}
 }
